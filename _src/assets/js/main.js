74a9ccb632419e2c56c965af17eaa96e97bedf20
Margui <margui.berjon@gmail.com>
2020-03-19 21:06:21 +0100

    Call Api function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('>> Ready :)');
+const inputSearch = document.querySelector('.js-input-search');
+const buttonSearch = document.querySelector('.js-button-search');
+const resultsSearch = document.querySelector('.js-results-search');
+
+let seriesResult = [];
+
+function getSeriesResult() {
+    const userSearch = inputSearch.value.toLowerCase();
+    fetch(`http://api.tvmaze.com/search/shows?q=${userSearch}`).then(response => response.json()).then(data => {
+        drawItems(data);
+    });
+}
+
+function drawItems(series) {
+    for (const serie of series) {
+        console.log(serie.show.name);
+    }
+}
+
+buttonSearch.addEventListener('click', getSeriesResult);
+
+
