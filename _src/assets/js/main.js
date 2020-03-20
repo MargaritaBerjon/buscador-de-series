1a0c340902a19b161be96926e448a3473b79f9a5
Margui <margui.berjon@gmail.com>
2020-03-20 13:16:04 +0100

    Favorite section

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -8,8 +8,11 @@ const resultsSearch = document.querySelector('.js-results-search');
 //Array para guardar resultados de la petición
 let seriesResult = [];
 
+
+
+
 //Función para hacer peticiones al API
-function getSeriesResult(ev) {
+function callSeriesResult(ev) {
   ev.preventDefault();
   const userSearch = inputSearch.value.toLowerCase();
   fetch(`http://api.tvmaze.com/search/shows?q=${userSearch}`).then(response => response.json()).then(data => {
@@ -33,11 +36,34 @@ function paintSeries(seriesResult) {
     } else {
       img.src = 'https://via.placeholder.com/210x295/ffffff/666666/?';
     }
+
+    li.classList.add('card', 'js-card');
     li.appendChild(title);
     li.appendChild(img);
-    li.classList.add('card');
     resultsSearch.appendChild(li);
     resultsSearch.classList.add('series-result');
+
+    //Seteo evento click
+    let cardlist = document.querySelectorAll('.js-card');
+    for (let card of cardlist) {
+      card.addEventListener('click', paintFavorites);
+    }
   }
 }
-buttonSearch.addEventListener('click', getSeriesResult);
+
+function paintFavorites(ev) {
+  const favoriteElem = ev.currentTarget;
+  const favoritesSection = document.querySelector('.js-favorites');
+  favoritesSection.appendChild(favoriteElem);
+}
+
+
+// function getFavoritesSeriesResult() {
+//   let favorites = localStorage.getItem('favorites') || '[]';
+//   const favoritesArr = JSON.parse(favorites);
+//   return favoritesArr;
+
+// }
+
+
+buttonSearch.addEventListener('click', callSeriesResult);
