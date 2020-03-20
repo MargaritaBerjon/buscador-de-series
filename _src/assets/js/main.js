d9ce01c5a1aea28ae51d4e4631c9f42c247d3a8c
Margui <margui.berjon@gmail.com>
2020-03-20 09:04:21 +0100

    Title and imag painting in html

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,21 +2,36 @@
 
 const inputSearch = document.querySelector('.js-input-search');
 const buttonSearch = document.querySelector('.js-button-search');
+//ul donde voy a pintar la lista de los resultados
 const resultsSearch = document.querySelector('.js-results-search');
 
+//Array para guardar resultados de la petición
 let seriesResult = [];
 
-function getSeriesResult() {
-    const userSearch = inputSearch.value.toLowerCase();
-    fetch(`http://api.tvmaze.com/search/shows?q=${userSearch}`).then(response => response.json()).then(data => {
-        drawItems(data);
-    });
+function getSeriesResult(ev) {
+  ev.preventDefault();
+  const userSearch = inputSearch.value.toLowerCase();
+  fetch(`http://api.tvmaze.com/search/shows?q=${userSearch}`).then(response => response.json()).then(data => {
+    seriesResult = data;
+    paintSeries(data);
+
+
+  });
 }
 
-function drawItems(series) {
-    for (const serie of series) {
-        console.log(serie.show.name);
-    }
+function paintSeries(seriesResult) {
+  for (const serie of seriesResult) {
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    const title = document.createElement('h3');
+
+    title.innerHTML = serie.show.name;
+    img.src = serie.show.image.medium;
+    li.appendChild(title);
+    li.appendChild(img);
+    resultsSearch.appendChild(li);
+  }
+
 }
 
 buttonSearch.addEventListener('click', getSeriesResult);
