9639f2d200f771fdf48d385b09458792db47180d
Margui <margui.berjon@gmail.com>
2020-03-20 11:52:44 +0100

    Call Api

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -8,32 +8,36 @@ const resultsSearch = document.querySelector('.js-results-search');
 //Array para guardar resultados de la petición
 let seriesResult = [];
 
+//Función para hacer peticiones al API
 function getSeriesResult(ev) {
   ev.preventDefault();
   const userSearch = inputSearch.value.toLowerCase();
   fetch(`http://api.tvmaze.com/search/shows?q=${userSearch}`).then(response => response.json()).then(data => {
     seriesResult = data;
     paintSeries(data);
-
-
   });
 }
 
+//Función para pintar resultados del API
+
 function paintSeries(seriesResult) {
+
   for (const serie of seriesResult) {
     const li = document.createElement('li');
     const img = document.createElement('img');
     const title = document.createElement('h3');
 
     title.innerHTML = serie.show.name;
-    img.src = serie.show.image.medium;
+    if (serie.show.image !== null) {
+      img.src = serie.show.image.medium;
+    } else {
+      img.src = 'https://via.placeholder.com/210x295/ffffff/666666/?';
+    }
     li.appendChild(title);
     li.appendChild(img);
+    li.classList.add('card');
     resultsSearch.appendChild(li);
+    resultsSearch.classList.add('series-result');
   }
-
 }
-
 buttonSearch.addEventListener('click', getSeriesResult);
-
-
